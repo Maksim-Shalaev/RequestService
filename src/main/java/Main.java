8a57e4fbbb0e5b1490dd35dcc7ae4b9b0259ee98
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-05-21 18:44:07 +0300

    Added Birthday class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 public class Main {
+    
     public static void main(String[] args) {
-    Post post = new Post();
-    post.name = "Maksim";
-    post.passport = "0001 # 123456";
-    post.patronymic = "Gennadievich";
-    post.phone = "+357 97 579799";
-    post.surname = "Shalaev";
-    post.subscription = true;
+        Post post = new Post();
+        post.name = "Maksim";
+        post.passport = "0001 # 123456";
+        post.patronymic = "Gennadievich";
+        post.phone = "+357 97 579799";
+        post.surname = "Shalaev";
+        post.subscription = true;
     }
 }
